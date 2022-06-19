cbba0d926e80ee1a8b86cb2d387e8bce440607b3
JoshuaOloton <olotonjoshua@gmail.com>
2022-06-19 19:07:25 +0000

    fixed pcchar error

--- pchar.c
+++ pchar.c
@@ -9,15 +9,15 @@
  */
 void pchar(stack_t **top, unsigned int line_no)
 {
-	if ((*top)->n < 0 || (*top)->n > 127)
-	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_no);
-		exit(EXIT_FAILURE);
-	}
 	if ((*top) == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_no);
 		exit(EXIT_FAILURE);
 	}
+	if ((*top)->n < 0 || (*top)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_no);
+		exit(EXIT_FAILURE);
+	}	
 	printf("%c\n", (*top)->n);
 }
